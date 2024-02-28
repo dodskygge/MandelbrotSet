36f35193ac5e7d6bcf131ea65fbdf717a8e473a0
dodskygge <makuch.oskar@gmail.com>
2024-02-28 13:21:19 +0100

    Update

--- MandelbrotSet/main.cpp
+++ MandelbrotSet/main.cpp
@@ -1,12 +1,12 @@
 #include <iostream>
 #include <fstream>
 
-using namespace std;
+//using namespace std;
 
 int findMandelbrot(double cr, double ci, int max_iterations) {
 	int i = 0;
 	double zr = 0.0, zi = 0.0;
-	
+
 	while (i < max_iterations && zr * zr + zi * zi < 4.0) {
 		double temp = zr * zr - zi * zi + cr;
 		zi = 2.0 * zr * zi + ci;
@@ -33,28 +33,35 @@ double mapToImaginary(int y, int imageHeight, double minI, double maxI) {
 
 int main() {
 	//Pobranie wartosci z pliku txt
-	ifstream fin("input.txt");
+	std::ifstream fin("input.txt");
 
 	int imageWidth, imageHeight, maxN;
 	double minR, maxR, minI, maxI;
+	std::string fileName;
 
 	//Sprawdzenie
 	if (!fin) {
-		cout << "Nie mozna odczytac pliku!" << endl;
-		cin.ignore();
+		std::cout << "Nie mozna odczytac pliku!" << std::endl;
+		std::cin.ignore();
 		return 0;
 	}
 
 	//FileInput
 	fin >> imageWidth >> imageHeight >> maxN;
 	fin >> minR >> maxR >> minI >> maxI;
-	fin.close(); //zamkniecie buforu
+	fin >> fileName;
+	fin.close();
+
+	//Sprawdzenie czy plik istnieje, jesli tak to zmiana nazwy
+	if (std::ifstream(fileName).good()) {
+		fileName = fileName.substr(0, fileName.size() - 4) + "_nowy.ppm";
+	}
 
 	//FileOutput
-	ofstream fout("fraktal.ppm");
-	fout << "P3" << endl; //Naglowek .ppm
-	fout << imageWidth << " " << imageHeight << endl; //Rozmiar
-	fout << "256" << endl; //Max wart. pikseli RGB
+	std::ofstream fout(fileName);
+	fout << "P3" << std::endl; //Naglowek .ppm
+	fout << imageWidth << " " << imageHeight << std::endl; //Rozmiar
+	fout << "256" << std::endl; //Max wart. pikseli RGB
 
 	//Dla kazdego pikselu
 	for (int y = 0; y < imageHeight; y++) { //Wiersz
@@ -68,20 +75,35 @@ int main() {
 			//Oblicz liczbe iteracji we wzorze Mandelbrota uzywajac wspomnianego c
 			int n = findMandelbrot(cr, ci, maxN);
 
-			//Mapowanie na kolory - moÅ¼na zmieniÄ‡ i pokombinowaÄ‡
-			int r = ( (int) sin(n)/n % 256);
-			int g = (  28  % 256);
-			int b = ( (int) cos(n) * n  % 256);
+			//Mapowanie na kolory - mo¿na zmieniæ i pokombinowaæ
+			//int r = ( (int) sin(n)*3 % 256);
+			//int g = ( (int) n/2  % 256 );
+			//int b = ( (int) cos(n)* n  % 256);
+			// 
+			int r = ( n*n % 256);
+			int g = ( 28 % 256);
+			int b = ( n % 256);
+			// 
+			//int r = ((int)sin(n) * n % 256);
+			//int g = ((int)n * 2 % 256);
+			//int b = ((int)cos(n) % 256);
+			//
+			//int r = ((int)n * n % 256);
+			//int g = (n * n % 256);
+			//int b = ((n * n) % 256);
+			// 
+			//int r = ((int)n * n % 256);
+			//int g = (n * n % 256);
+			//int b = ((n * n) % 256);
 
 			//Wyjscie do obrazka
 			fout << r << " " << g << " " << b << " ";
 		}
-		cout << "\033[H" << y << " / " << imageHeight << " px" << " X " << imageWidth << " px";
-		fout << endl;
+		std::cout << "\033[H" << y << " / " << imageHeight << " px" << " X " << imageWidth << " px";
+		fout << std::endl;
 	}
 
 	fout.close();
-	cout << endl << "Zakonczono program pomyslnie!" << endl;
+	std::cout << std::endl << "Zakonczono program pomyslnie!" << std::endl;
 	return 0;
-
 }
